d8e1838d9795459df9497c5d85472705f40a6fe2
seibo-hub <airwhite2525@gmail.com>
2025-05-10 15:15:05 +0900

    channelsのページレイアウト完成

--- ChatApp/app.py
+++ ChatApp/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template ('index.html')
 
+@app.route('/channels', methods=['GET'])
+def channels():
+    return render_template ('channels.html')
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
